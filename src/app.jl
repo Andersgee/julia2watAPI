8ca552becce814cec362b724401f16cdcdb3a14e
andy <andersgee@gmail.com>
2020-06-21 20:17:35 +0200

    rawpayload() aka string

--- src/app.jl
+++ src/app.jl
@@ -21,6 +21,10 @@ function launchServer(port)
 	  json("Hello $(jsonpayload()["name"])")
 	end
 
+	route("/text", method = POST) do
+	  julia2wat.code_wat(rawpayload())
+	end
+
     Genie.AppServer.startup()
 end
 
--- src/julia2wat.jl
+++ src/julia2wat.jl
@@ -1,12 +1,15 @@
 module julia2wat
-	greet() = "Hello from module"
 
 
-	function code_wat(str)
-		result = eval(Meta.parse(str))
-		
-		ex=Meta.parse(str).args[end] #only lower the last expression in the code block.
-		cinfo, R = code_typed(eval(ex.args[1]), Base.typesof(eval(ex.args[2:end]...)), optimize=true)[1]
-		return string(cinfo.code)
-	end
+
+greet() = "Hello from module"
+
+function code_wat(str)
+	result = eval(Meta.parse(str))
+	
+	ex=Meta.parse(str).args[end] #only lower the last expression in the code block.
+	cinfo, R = code_typed(eval(ex.args[1]), Base.typesof(eval(ex.args[2:end]...)), optimize=true)[1]
+	return string(cinfo.code)
+end
+
 end	
